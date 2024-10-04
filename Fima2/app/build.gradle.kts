d4f7efbde8c4c0cbf5f98af8daa8924cc14b716c
jonaa <56060812+jaflanet@users.noreply.github.com>
2024-10-04 14:22:21 +0700

    camera feature

--- Fima2/app/build.gradle.kts
+++ Fima2/app/build.gradle.kts
@@ -40,6 +40,10 @@ android {
 
 dependencies {
 
+    implementation (libs.androidx.camera.core)
+    implementation (libs.androidx.camera.camera2)
+    implementation (libs.androidx.camera.lifecycle)
+    implementation (libs.androidx.camera.view)
     implementation(libs.play.services.location)
     implementation(libs.retrofit)
     implementation(libs.converter.gson)
