86a1644a4a2cde9533c9045c3348382670726c04
Bryan Felix <bryanfelix2001@gmail.com>
2024-10-05 09:56:03 +0700

    reset attendance button

--- Fima2/app/src/main/java/com/example/fima_2/ui/profile/ProfileFragment.kt
+++ Fima2/app/src/main/java/com/example/fima_2/ui/profile/ProfileFragment.kt
@@ -1,9 +1,12 @@
 package com.example.fima_2.ui.profile
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.fima_2.databinding.FragmentProfileBinding
@@ -16,6 +19,9 @@ class ProfileFragment : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
 
+    private lateinit var resetBtn: Button
+    private lateinit var sharedPreferences: SharedPreferences
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -26,6 +32,20 @@ class ProfileFragment : Fragment() {
         val root: View = binding.root
 
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        sharedPreferences = requireActivity().getSharedPreferences("MyPrefs", Context.MODE_PRIVATE)
+
+        resetBtn = binding.resetAttendanceBtn
+        resetBtn.setOnClickListener {
+            val editor = sharedPreferences.edit()
+            editor.putString("clockInTimeString", null) // Reset clock in time
+            editor.putString("clockOutTimeString", null) // Reset clock out time
+            editor.putString("attendanceStatus", "absent") // Reset attendance status
+            editor.apply()
+        }
     }
 
     override fun onDestroyView() {
